b245072ac226684bc3d0bbb33a7eacff8c0432f5
Jamal Mahmoud <c13730921@mydit.ie>
2016-02-23 15:22:53 +0000

    empty class implementation of clap behaviour

---
+++ Clap.java
@@ -0,0 +1,25 @@
+import lejos.subsumption.Behaviour;
+import lejos.subsumption.Arbitrator;
+
+public class Claps implements Behaviour () {
+	//fields
+	private ;
+	
+	//constructor 
+	public Claps() {
+		
+	}
+	//Methods
+	public void action() {
+		//implement actions here
+	}
+	
+	public suppress() {
+		//implement suppression here
+	}
+	
+	public takeControl() {
+			
+	}
+
+}
