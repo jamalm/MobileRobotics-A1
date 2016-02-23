20cd5f9c309e0c5fb91d35a586442c525f7a547a
Jamal Mahmoud <c13730921@mydit.ie>
2016-02-23 15:25:00 +0000

    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -1,18 +1,18 @@
 import lejos.subsumption.Arbitrator;
-import lejos.subsumption.Behaviour;
+import lejos.subsumption.Behavior;
 
 public class Assignment {
 	
 	public static void main(String [] args) {
 		
 		//set up behaviours
-		Behaviour step1 = new Clap();
-		Behaviour step2 = new LightDetect();
-		Behaviour step3 = new SonarDetect();
-		Behaviour step4 = new HitWall();
+		Behavior step1 = new Clap();
+		Behavior step2 = new LightDetect();
+		Behavior step3 = new SonarDetect();
+		Behavior step4 = new HitWall();
 		
 		//behaviour list
-		Behaviour [] steps = {step4, step3, step2 ,step1};
+		Behavior [] steps = {step4, step3, step2 ,step1};
 		
 		//arbitrator instance to run behaviours
 		Arbitrator control = new Arbitrator(steps);
