498c607b53cd66def1caa9707213f1d8a3242666
Jamal Mahmoud <c13730921@mydit.ie>
2016-03-15 15:06:51 +0000

    updated comments

--- Assignment.java
+++ Assignment.java
@@ -1,17 +1,25 @@
 import lejos.robotics.subsumption.*;
 import lejos.nxt.*;
-
+/*
+ * Assignment 1 for mobile robotics 
+ * Implementing the Behavior API, allow the Arbitrator to carry out behaviors
+ * depending on the conditions
+ *
+ * @author Jamal Mahmoud
+ * @author Minni Hiltunen
+ */
 public class Assignment {
 	
 	public static void main(String [] args) {
 		
-		//set up behaviours
+		//initialise behaviors
 		Behavior step1 = new Clap();
 		Behavior step2 = new LightDetect();
 		Behavior step3 = new SonarDetect();
 		Behavior step4 = new HitWall();
 		
-		//behaviour list
+		
+		//behavior array that will be passed to the arbitrator class
 		Behavior [] steps = {step1, step2, step3, step4};
 		
 		//arbitrator instance to run behaviours
